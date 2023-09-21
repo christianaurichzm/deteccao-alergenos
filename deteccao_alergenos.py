2eaf5944d176b3a96157518da39a6b7f2d6caaf5
Christian Aurich <christian.aurichzm@gmail.com>
2023-09-21 01:18:43 -0300

    feat: adiciona retroalimentação da ontologia

--- deteccao_alergenos.py
+++ deteccao_alergenos.py
@@ -71,14 +71,20 @@ def is_allergen_present(ingredient, allergen, algorithm):
             return bert_similarity(ingredient, allergen) > 0.92
 
 
-def detect_allergens(ingredients, allergens_set, algorithm):
-    detected_allergens = [ingredient for ingredient in ingredients
-                          if any(is_allergen_present(ingredient, allergen, algorithm) for allergen in allergens_set)]
+def detect_allergens(ingredients, cleaned_allergens_set, allergen_mapping, algorithm):
+    detected_allergens = []
+    for ingredient in ingredients:
+        for cleaned_allergen in cleaned_allergens_set:
+            if is_allergen_present(ingredient, cleaned_allergen, algorithm):
+                original_allergen = allergen_mapping.get(cleaned_allergen, cleaned_allergen)
+                detected_allergens.append((ingredient, original_allergen))
+                break
     return detected_allergens
+
 
 def clean_text(text):
     cleaned = unidecode(text.lower().strip())
-    cleaned = re.sub(r'[^a-zA-Z\s,]', '', cleaned)
+    cleaned = re.sub(r'[^a-zA-Z\s,]', ' ', cleaned)
     cleaned = re.sub(r',+', ',', cleaned)
     cleaned = re.sub(r' +', ' ', cleaned)
     return cleaned
@@ -90,15 +96,23 @@ def preprocess_data(df):
     return df
 
 
-
-def load_allergens_from_ontology(ontology_path, query_path="query_alergenos.sparql"):
-    get_ontology(ontology_path).load()
-    sync_reasoner()
+def load_allergens_from_ontology(query_path):
     with open(query_path, "r") as file:
         query = file.read()
     results = list(default_world.sparql(query))
-    allergens = set(clean_text(label) for [label] in results)
+    allergens = set(label for [label] in results)
     return allergens
+
+
+def create_allergen_in_ontology(ontology, ontology_path, derived_name, allergen_base):
+    allergen_base_instance = ontology.search_one(label=allergen_base)
+    AlergenoPorDerivacao = ontology.search_one(label="Alérgeno por Derivação")
+
+    new_allergen = AlergenoPorDerivacao(derived_name.title().replace(" ", ""))
+    new_allergen.label = [derived_name.title()]
+    new_allergen.eDerivadoDe.append(allergen_base_instance)
+
+    ontology.save(ontology_path)
 
 
 def main():
@@ -106,13 +120,40 @@ def main():
     df = load_data('openfoodfacts_export.csv')
     df = preprocess_data(df)
 
-    allergens_set = load_allergens_from_ontology("ontologia.owl")
+    ONTOLOGY_PATH = "ontologia.owl"
+
+    ontology = get_ontology(ONTOLOGY_PATH).load()
+    sync_reasoner()
+
+    allergens_set = load_allergens_from_ontology("query_alergenos.sparql")
+    cleaned_allergens_set = set()
+    allergen_mapping = {}
+
+    for allergen in allergens_set:
+        cleaned = clean_text(allergen)
+        cleaned_allergens_set.add(cleaned)
+        allergen_mapping[cleaned] = allergen
+
+    extracted_ingredients = df['ingredients_text_pt'].progress_apply(extract_ingredients)
+
+    detected_allergens_match = extracted_ingredients.progress_apply(
+        lambda x: detect_allergens(x, cleaned_allergens_set, allergen_mapping, Algorithms.MATCH)
+    )
+
+    added_derived_allergens = set()
+
+    for allergen_pairs in detected_allergens_match:
+        for derivated_allergen, base_allergen in allergen_pairs:
+            if derivated_allergen != base_allergen and derivated_allergen not in added_derived_allergens and not ontology.search_one(
+                    label=derivated_allergen.title()):
+                create_allergen_in_ontology(ontology, ONTOLOGY_PATH, derivated_allergen, base_allergen)
+                added_derived_allergens.add(derivated_allergen)
 
     for algorithm in Algorithms:
-        df[f'alergenos_{algorithm.value}'] = df['ingredients_text_pt'].progress_apply(
-            lambda x: detect_allergens(extract_ingredients(
-                x), allergens_set, algorithm)
+        detected_allergens = extracted_ingredients.progress_apply(
+            lambda x: detect_allergens(x, cleaned_allergens_set, allergen_mapping, algorithm)
         )
+        df[f'alergenos_{algorithm.value}'] = detected_allergens.apply(lambda x: [detected[0] for detected in x])
 
     return df
 
